56aa42c99179f798e4811a2e4cff554760bab83a
catherin <catherinr24@gmail.com>
2024-04-23 14:54:15 -0400

    improving the code I have so far

--- main.py
+++ main.py
@@ -2,18 +2,51 @@
 This is the main code of my program, Guess the number !!!
 """
 import random
+import time
+# import unittest
 
-# selectores
-secretNumber= random.randrange(1, 100)
-flag= True
+# Functions
+def get_guess_Player():
+       print("##### Player's turn ####")
+       time.sleep(2)
+       number= int(input("I'ts your turn, please guess the number, write it down here:"))
+       print(number)
+       return number
 
+def get_guess_computer():
+      print("##### Computer's turn ####")
+      time.sleep(2)
+      computerNumber= random.randint(1, 100)
+      print(computerNumber)
+      return computerNumber
+   
+def mainFunction():
+    secretNumber= random.randint(1, 100)
+    flag= True
+    while flag:
+        player_guess= get_guess_Player()
+        time.sleep(2)
+        if player_guess == secretNumber:
+            print("Congratulations! You won...")
+            flag= False
+            break
+        else:
+            if player_guess > secretNumber:
+                print('Too higth')
+            else:
+                print('Too low')
 
-# main function
-while flag:
-    name= int(input("I'ts your turn, please guess the number, write it down here:"))
-    print(name)
-    if name == secretNumber:
-        print("Congratulations! You won...")
-        flag= False
-    else:
-        print('Sorry, keep trying')
+        computer_guess= get_guess_computer()
+        time.sleep(2)
+        if computer_guess == secretNumber:
+            print("Congratulations! You won...")
+            flag= False
+            break
+        else:
+            if computer_guess > secretNumber:
+                print('Too higth')
+            else:
+                print('Too low')
+       
+
+mainFunction()
