7e9d109a805a7ca11b026f60c8e31869d0217290
catherin <catherinr24@gmail.com>
2024-04-29 15:49:57 -0400

    updating with Hito3 and removing pycache from git

--- app.py
+++ app.py
@@ -8,21 +8,20 @@ import time
 #Create two empty list which I will use in the main function
 guesses_computer= []
 guesses_player= []
+secretNumber= random.randint(1, 100) #Create a variable witch contains the secret number
 
 # This function get the player guess from the input
-def get_guess_Player():
-       print("##### Player's turn ####")
+def get_guess_player():
        time.sleep(2)
-       number= int(input("I'ts your turn, please guess the number, write it down here: "))
-       print(number)
+       global number
+       number= int(input("I'ts your turn, please guess the number, write it down here:"))
        return number
 
 # This function get the computer guess from the random method
 def get_guess_computer():
-      print("##### Computer's turn ####")
       time.sleep(2)
+      global computerNumber
       computerNumber= random.randint(1, 100)
-      print(computerNumber)
       return computerNumber
 
 # This funtion display the list of guesses of the winer
@@ -31,48 +30,47 @@ def show_guesses_winer(iterable):
     for guess in iterable:
         print(guess, end=" ")
     print()
-   
-# This is the principal function, which is in charge to execute the loop for the game
-def mainFunction(secretNumber=None):
 
+#This function evaluate if the guess player o computer is equal to secret number and return true or false
+def evaluate_guess(guess):
+    if guess == secretNumber:
+        print("Congratulations! You won...")
+        return True
+    elif guess > secretNumber:
+        print('Too high')
+    else:
+        print('Too low')
+    return False
+    
+# This is the principal function, which is in charge to execute the loop for the game 
+def mainFunction():    
     global guesses_computer, guesses_player 
+    flag_for_break_loop= True
 
-    if secretNumber is None:
-        secretNumber= random.randint(1, 100)
-
-    flag= True
-
-    while flag:
-        player_guess= get_guess_Player()
+    while flag_for_break_loop:
+        print("##### Player's turn ####")
+        player_guess= get_guess_player()
         guesses_player.append(player_guess)
+        
         time.sleep(2)
-        if player_guess == secretNumber:
-            print("Congratulations! You won...")
-            flag= False
+        if evaluate_guess(player_guess):
+            flag_for_break_loop= False
             show_guesses_winer(guesses_player)
             break
-        else:
-            if player_guess > secretNumber:
-                print('Too high')
-            else:
-                print('Too low')
-
+        
+        
+        print("#### Computer's turn ####")
         computer_guess= get_guess_computer()
+        print(f"It's the computer's turn: {computer_guess} ")
         guesses_computer.append(computer_guess)
         time.sleep(2)
-        if computer_guess == secretNumber:
-            print("Congratulations! You won...")
-            flag= False
+        if evaluate_guess(computer_guess):
+            flag_for_break_loop= False
             show_guesses_winer(guesses_computer)
             break
-        else:
-            if computer_guess > secretNumber:
-                print('Too higth')
-            else:
-                print('Too low')
-        
-if __name__ == '__main__':
-    mainFunction()
+
+     #This return will be used in my test     
+    return flag_for_break_loop
 
 #This block of code ask the player if they want to play again or not
 def ask_play_again():
@@ -83,4 +81,10 @@ def ask_play_again():
         print('See you later')
         print()
         return;
-ask_play_again()
+      
+
+if __name__ == '__main__':
+    mainFunction()
+    ask_play_again()
+
+
